0a7e12843c290e9cb3be06e3fb5dc0de13ce311a
msielicki <thirrasch@gmail.com>
2022-07-03 18:34:35 +0200

    Add Logger module interface

---
+++ engine/source/Logger/FlavoEngine.Logger.cpp
@@ -0,0 +1,52 @@
+module;
+
+module FlavoEngine.Logger;
+
+//import <string>;
+//
+//namespace FlavoEngine::Logger
+//{
+//	template<typename ...Args>
+//	void debug(std::string_view format, Args... args)
+//	{
+//
+//	}
+//
+//	void info()
+//	{
+//
+//	}
+//
+//	void warning()
+//	{
+//
+//	}
+//
+//	void error()
+//	{
+//
+//	}
+//}
+//
+//module :private;
+//
+//namespace FlavoEngine::Logger
+//{
+//	class Logger
+//	{
+//	public:
+//		static Logger& GetInstance();
+//
+//		template<typename ...Args>
+//		void Log(std::string_view type, std::string_view message, Args... args);
+//
+//	private:
+//		Logger();
+//		Logger(const Logger&) = delete;
+//		Logger(Logger&&) = delete;
+//		~Logger();
+//		Logger& operator=(const Logger&) = delete;
+//		Logger& operator=(Logger&&) = delete;
+//
+//	};
+//}
---
+++ engine/source/Logger/FlavoEngine.Logger.ixx
@@ -0,0 +1,11 @@
+module;
+
+export module FlavoEngine.Logger;
+
+export namespace FlavoEngine::Logger
+{
+	void debug();
+	void info();
+	void warning();
+	void error();
+}
