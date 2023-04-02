cc466c35e4e35694c613b545afc9c7ba82956e76
msielicki <thirrasch@gmail.com>
2023-04-02 14:56:38 +0200

    Add new aliases to FTL

---
+++ engine/source/core/ftl/algorithm.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <algorithm>
+
+namespace flavo::ftl
+{
+    using std::max;
+    using std::min;
+}
---
+++ engine/source/core/ftl/tuple.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <tuple>
+
+namespace flavo::ftl
+{
+	using std::tuple;
+	using std::tuple_element;
+}
